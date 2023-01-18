f0f6f141dbc8a0771fef66bd0198ebc864addf37
Luciano <lucianocramero@gmail.com>
2023-01-18 10:34:42 -0300

    commiteo los cambios, agregado de fetch y promise

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -2,6 +2,8 @@
             Importaciones
 ####################################*/
 //Modulos
+import { useState } from "react"
+
 //Estilos
 import "./ItemList.css"
 //Componentes
@@ -10,91 +12,26 @@ import Item from "../item/Item"
             Logica
 ####################################*/
 const ItemList = () => {
-    const listaDeProductos = [
-        {
-            id: 1,
-            nombre: "Rafting",
-            descripcion: "Descensos guiados por el río en grupos de hasta 6 personas",
-            precio: 40,
-            categoria: "actividad exteriores agua",
-            stock: 18,
-            image: "./img/rafting.jpeg"
-        },
-        {
-            id: 2,
-            nombre: "Kayak",
-            descripcion: "Recorridos guiados por el lago en grupos o individuales",
-            precio: 35,
-            categoria: "actividad exteriores agua",
-            stock: 20,
-            image: "./img/kayak.jpg"
-        },
-        {
-            id: 3,
-            nombre: "Catamaran",
-            descripcion: "Recorridos guiados por el lago en grupos de hasta 12 personas",
-            precio: 25,
-            categoria: "actividad exteriores agua",
-            stock: 25,
-            image: "./img/catamaran.jpg"
-        },
-        {
-            id: 4,
-            nombre: "Diving",
-            descripcion: "Exploraciones guiadas en el lago en grupos de hasta 6 personas",
-            precio: 45,
-            categoria: "actividad exteriores agua",
-            stock: 19,
-            image: "./img/diving.webp"
-        },
-        {
-            id: 5,
-            nombre: "Trekking",
-            descripcion: "Recorridos guiados por senderos en grupos de hasta 24 personas",
-            precio: 20,
-            categoria: "actividad exteriores tierra",
-            stock: 16,
-            image: "./img/trekking.webp"
-        },
-        {
-            id: 6,
-            nombre: "Climb",
-            descripcion: "Ascensos guiados por la montaña en grupos reducidos",
-            precio: 40,
-            categoria: "actividad exteriores tierra",
-            stock: 21,
-            image: "./img/climb.jpg"
-        },
-        {
-            id: 7,
-            nombre: "Zip line",
-            descripcion: "Recorridos individuales a través de montañas y lagos",
-            precio: 15,
-            categoria: "actividad exteriores tierra",
-            stock: 23,
-            image: "./img/zipline.jpg"
-        },
-        {
-            id: 8,
-            nombre: "Cave expedition",
-            descripcion: "Exploraciones guiadas a través de cuevas en grupos de hasta 6 personas",
-            precio: 60,
-            categoria: "actividad exteriores tierra",
-            stock: 12,
-            image: "./img/cave-expedition.webp"
-        }
-    ]
-    const listaDeProductosRend = listaDeProductos.map( p => <Item data = {p}/> )
+    const [Render, setRender] = useState ([])    
+/*Añadimos la Promise, la funcion de esta es traer la informacion una vez que esta se cumple*/
+    const devolucion = new Promise ((resolve, /* reject */) => {
+        resolve(
+            fetch ("./../data/Data.json")
+                .then(res => res.json())
+                .then(json => json.map( p => <Item data = {p}/>))
+        )
+    })
+        devolucion.then((data) => {setRender(data)})
+
     return(
         <div className="div-itemList">
             <h2>Productos</h2>
             <div className="div-itemList-render">
-                {listaDeProductosRend}
+                {Render}
             </div>
         </div>
     )
 }
-
 /*##################################
             Exportraciones
 ####################################*/
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -15,17 +15,23 @@ const NavBar = (props) => {
     return(
         <header className="navbar">
             <div className="navbar-header">
-                <img src="./img/seasonal.webp"></img>
                 <a href="index.js" className="navbar-titulo">
                     {props.nombre}
                 </a>
             </div>
             <div className="navbar-div">
                 <div className="navbar-div-ul">
+                    <input type="checkbox" id="check-menu"></input>
+                    <label htmlFor="check-menu" id="label-check">
+                        <svg class="svg-icon" className="label-icon" viewBox="0 0 20 20" stroke="#fff">
+						    <path d="M10,1.445c-4.726,0-8.555,3.829-8.555,8.555c0,4.725,3.829,8.555,8.555,8.555c4.725,0,8.555-3.83,8.555-8.555C18.555,5.274,14.725,1.445,10,1.445 M10,17.654c-4.221,0-7.654-3.434-7.654-7.654c0-4.221,3.433-7.654,7.654-7.654c4.222,0,7.654,3.433,7.654,7.654C17.654,14.221,14.222,17.654,10,17.654 M14.39,10c0,0.248-0.203,0.45-0.45,0.45H6.06c-0.248,0-0.45-0.203-0.45-0.45s0.203-0.45,0.45-0.45h7.879C14.187,9.55,14.39,9.752,14.39,10 M14.39,12.702c0,0.247-0.203,0.449-0.45,0.449H6.06c-0.248,0-0.45-0.202-0.45-0.449c0-0.248,0.203-0.451,0.45-0.451h7.879C14.187,12.251,14.39,12.454,14.39,12.702 M14.39,7.298c0,0.248-0.203,0.45-0.45,0.45H6.06c-0.248,0-0.45-0.203-0.45-0.45s0.203-0.45,0.45-0.45h7.879C14.187,6.848,14.39,7.051,14.39,7.298">
+                            </path>
+						</svg>
+                    </label>
                     <nav>
                         <ul className="navbar-ul">
                             <li>
-                                <a className="navbar-ul-text" href="index.js">
+                                <a className="" href="index.js">
                                     Inicio
                                 </a>
                             </li>
